785a6dc00e4a31113693ab0bc6127086c49c6531
Andres Baumeler <9219885+abaumeler@users.noreply.github.com>
2022-01-09 19:27:28 +0100

    added login form

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,16 +1,46 @@
-import { ChakraProvider, theme, Text, Heading, Stack, Center } from '@chakra-ui/react';
+import {  ChakraProvider, 
+          theme, 
+          Text, 
+          Button, 
+          Input,
+          Heading, 
+          Stack, 
+          Center,
+          FormControl,
+          FormLabel,
+          FormHelperText } from '@chakra-ui/react';
+import { useState } from "react";
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 
-const LoginPage = () => (
+function LoginPage(){
+
+  function LoginForm(){
+    const[name, SetName] = useState();
+  }
+
+  return (
     <ChakraProvider theme={theme}>
       <Center h="100vh" bg="blue.200">
         <Stack boxShadow="md" bg="whiteAlpha.900" p="20" rounded="md">
-          <img src="reshot-icon-food-equipment.svg" maxW="70px" mb="8" mx="auto" />
+          <img src="reshot-icon-food-equipment.svg" maxW="20px" mb="10" mx="auto" />
           <Heading as="h1">Log in.</Heading>
-          <Text fontsize="lg" color="gray.600">Please log in</Text>
+          <Text fontsize="lg" color="gray.600">Please log in to access FoodPlanner</Text>
+          <FormControl isRequired>
+            <FormLabel htmlFor='email'>Email address</FormLabel>
+            <Input id='email' type='email' placeholder='E-Mail' />
+            <FormLabel htmlFor='email'>Password</FormLabel>
+            <Input id='password' type='password' placeholder='Password' />
+          </FormControl>
+          
+          <Stack justify="center" color="gray.600" spacing="3">
+            <Text as="div" textAlign="center">
+              <span>Don&lsquo;t have an account? </span><Button colorScheme="blue" variant="link">Sign Up</Button>
+            </Text>
+            <Button colorScheme="blue" variant="link">Forgot password?</Button>
+          </Stack>
         </Stack>
       </Center>
     </ChakraProvider>
-)
-
+  );
+}
 export default LoginPage;
