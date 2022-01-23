c443067248c3196efe9efd16fcc9e8e44b924871
Andres Baumeler <9219885+abaumeler@users.noreply.github.com>
2022-01-23 17:46:50 +0100

    - implemented colorMode switcher - extended default theme - removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
 import LoginPage from './LoginPage';
 
 function App() {
--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,6 +1,4 @@
-import {  ChakraProvider, 
-          theme, 
-          Text, 
+import {  Text, 
           Button, 
           Input,
           Heading, 
@@ -8,42 +6,38 @@ import {  ChakraProvider,
           Center,
           FormControl,
           FormLabel,
-          FormHelperText } from '@chakra-ui/react';
-import { useState } from "react";
+          useColorModeValue  } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 
-function LoginPage(){
 
-  function LoginForm(){
-    const[name, SetName] = useState();
-  }
+function LoginPage(){
+  const bg =  useColorModeValue('gray.100', 'gray.900')
+  const logo = useColorModeValue('reshot-icon-food-equipment_light.png', 'reshot-icon-food-equipment_dark.png')
 
   return (
-    <ChakraProvider theme={theme}>
-      <Center h="100vh" bg="blue.200">
-        <Stack boxShadow="md" bg="whiteAlpha.900" p="20" rounded="md">
-          <img src="reshot-icon-food-equipment.svg" maxW="20px" mb="10" mx="auto" />
+      <Center h="100vh">
+        <Stack boxShadow="2xl" p="20" rounded="md" bg={bg}>
+          <img src={logo} maxW="20px" mb="10" mx="auto" alt="logo"/>
           <Heading as="h1">Log in.</Heading>
-          <Text fontsize="lg" color="gray.600">Please log in to access FoodPlanner</Text>
+          <Text fontsize="lg">Please log in to access FoodPlanner</Text>
           <FormControl isRequired>
             <FormLabel htmlFor='email'>Email address</FormLabel>
             <Input id='email' type='email' placeholder='E-Mail' />
             <FormLabel htmlFor='email'>Password</FormLabel>
             <Input id='password' type='password' placeholder='Password' />
           </FormControl>
-          <Button type="submit" variantColor="blue.200" width="full" mt={4}>
+          <Button type="submit" width="full" mt={4}>
             Sign In
-          </Button>
-          
-          <Stack justify="center" color="gray.600" spacing="3">
+          </Button>       
+          <Stack justify="center" spacing="3">
             <Text as="div" textAlign="center">
-              <span>Don&lsquo;t have an account? </span><Button colorScheme="blue" variant="link">Sign Up</Button>
+              <span>Don&lsquo;t have an account? </span><Button variant="link">Sign Up</Button>
             </Text>
-            <Button colorScheme="blue" variant="link">Forgot password?</Button>
+            <Button variant="link">Forgot password?</Button>
           </Stack>
+          <ColorModeSwitcher></ColorModeSwitcher>
         </Stack>
       </Center>
-    </ChakraProvider>
   );
 }
 export default LoginPage;
